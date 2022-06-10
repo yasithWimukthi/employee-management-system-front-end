6f99fabbdefb4662dbd05edc02e454fb2e002687
yasith <it19966922@my.sliit.lk>
2022-06-10 22:39:58 +0530

    create redux store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import './App.css';
-import 'antd/dist/antd.css';
-import TopNavBar from "./components/shared/TopNavBar/TopNavBar";
-import LoginPage from "./pages/Login/Login";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import NotFound from "./components/404/NotFound";
+import DashBoard from "./components/DashBoard/DashBoard";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
 
-function App() {
-  return (
-    <div>
-      <TopNavBar />
-        <LoginPage/>
-    </div>
+const App = () => {
+  const isLoggetIn = useSelector((state) => state.login.isLoggedIn);
+
+  const loggedInRoutes = (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/*" element={<NotFound/>} />
+    </Routes>
   );
-}
+
+  const loggedOutRoutes = (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/*" element={<NotFound/>} />
+    </Routes>
+  );
+
+  return isLoggetIn ? loggedInRoutes : loggedOutRoutes;
+};
 
 export default App;
